132a9ee5ee629b8424193c68751e4d155a248005
Administrador <Administrador@UTP095616-2.grupoutp.net>
2019-05-11 16:48:39 -0500

    Primer commit a mi repositorio

---
+++ pruebaRepoImportado/src/main/java/idat/edu/pe/daa2/pruebarepoimportado/BeanPrueba.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package idat.edu.pe.daa2.pruebarepoimportado;
+
+/**
+ *
+ * @author Administrador
+ */
+public class BeanPrueba {
+    
+}
